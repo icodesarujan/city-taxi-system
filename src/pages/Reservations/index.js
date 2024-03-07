19b3f8188f3b16f3ff0ac4fd3c560886f30d7a27
ZiyahulAnsath <91413647+ZiyahulAnsath@users.noreply.github.com>
2024-03-07 08:09:08 +0530

    Reservation Components are completed

--- src/pages/Reservations/index.js
+++ src/pages/Reservations/index.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { Button, Container, Table } from "react-bootstrap";
+import { tableData, tableHeading } from "../../Data/ReservationData";
+
+const getClassNames = (key, value) => {
+  if (key === "PaymentProcess") {
+    if (value === "Pending") {
+      return "bg-warning text-black fw-bolder rounded text-center";
+    } else if (value === "Success") {
+      return "bg-success text-white fw-bolder rounded text-center";
+    }
+  }
+  return "";
+};
 
 const Reservations = () => {
-  return (
-    <div>Reservations</div>
-  )
-}
+  const onActionClick = (rowData) => {
+    console.log("Row data:", rowData);
+  };
 
-export default Reservations
+  return (
+    <div>
+      <Container fluid>
+        <h1 className="pb-4">Reservations Details</h1>
+        <Table responsive className="text-center">
+          <thead>
+            <tr>
+              {tableHeading?.map((heading, index) => (
+                <th key={index}>{heading}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {tableData?.map((item, rowIndex) => (
+              <tr key={rowIndex}>
+                {Object.entries(item).map(([key, value], cellIndex) => (
+                  <td key={cellIndex} className={getClassNames(key, value)}>
+                    {value}
+                  </td>
+                ))}
+                <td>
+                  <Button
+                    variant="outline-primary"
+                    onClick={() => onActionClick(item)}
+                  >
+                    View {item.ReservationID}
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
+  );
+};
+
+export default Reservations;
