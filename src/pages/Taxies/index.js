898aa55b36bee616107cd2e9a5d7b3c4a2cdd365
ZiyahulAnsath <91413647+ZiyahulAnsath@users.noreply.github.com>
2024-03-17 14:33:57 +0530

    Texi table Improvement

--- src/pages/Taxies/index.js
+++ src/pages/Taxies/index.js
@@ -1,49 +1,70 @@
-import React from 'react'
-import { supabase } from '../../utils/supabase'
-import { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
-import Taxi from '../../models/Taxi';
+import React from "react";
+import { supabase } from "../../utils/supabase";
+import { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+import Taxi from "../../models/Taxi";
 
 const Taxies = () => {
-  const [taxies, setTaxies] = useState([])
+  const [taxies, setTaxies] = useState([]);
   useEffect(() => {
     async function getTaxies() {
-      const { data: taxies } = await supabase.from('taxies').select()
+      const { data: taxies } = await supabase.from("taxies").select();
       if (taxies.length > 0) {
-        setTaxies(Taxi.fromTaxies(taxies))
+        setTaxies(Taxi.fromTaxies(taxies));
       }
     }
 
-    getTaxies()
-  }, [])
+    getTaxies();
+  }, []);
   return (
     <>
       <Table striped bordered hover size="sm">
         <thead>
           <tr>
             <th>#</th>
-            <th>Vehicle name</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+            <th>Color</th>
             <th>Vehicle number</th>
-            <th>Driver name</th>
+            <th>Driver Name</th>
+            <th>Contact</th>
+            <th>Licence No</th>
+            <th>Availability</th>
           </tr>
         </thead>
         <tbody>
-          {taxies.map(taxi => {
+          {taxies.map((taxi) => {
             return (
-            <tr key={taxi.id}>
-              <td>{taxi.id}</td>
-              <td>{taxi.name}</td>
-              <td>{taxi.vehicleNo}</td>
-              <td>{taxi.driverName}</td>
-            </tr>
-
-            )
-
+              <tr key={taxi.id}>
+                <td>{taxi.id}</td>
+                <td>{taxi.make}</td>
+                <td>{taxi.model}</td>
+                <td>{taxi.year}</td>
+                <td>{taxi.color}</td>
+                <td>{taxi.vehicleNo}</td>
+                <td>{taxi.driverName}</td>
+                <td>{taxi.contact}</td>
+                <td>{taxi.licNumber}</td>
+                <td>{taxi.availability ? "Available" : "Not-Available"}</td>
+              </tr>
+            );
           })}
         </tbody>
       </Table>
     </>
-  )
-}
+  );
+};
 
-export default Taxies
+export default Taxies;
+// name,
+//     vehicleNo,
+//     driverName,
+//     make,
+//     model,
+//     year,
+//     color,
+//     contact,
+//     licNumber,
+//     expDate,
+//     availability,
